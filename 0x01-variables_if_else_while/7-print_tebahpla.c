1840ceaad95ab67305d57c8192d38f410e1aa92d
PeterMaar <maarpeter99@gmail.com>
2023-03-20 08:10:29 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - prints the lowercase alphabet in reverse.
+* main - prints the lowercase alphabet in reverse
 * Return: always 0
 */
 int main(void)
@@ -8,6 +8,7 @@ int main(void)
 char ch;
 for (ch = 'Z'; ch >= 'a'; ch--)
 putchar(ch);
+
 putchar('\n');
 return (0);
 }
