fc86e7b171839c3c97d741f80ffbee8248687b10
PeterMaar <maarpeter99@gmail.com>
2023-03-21 06:05:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - main function
+* print_alphabet - print all
+* Return: always 0
+*/
+
+int main(void)
+{
+char letter;
+for (letter = 'a' ; letter <= 'z' ; letter++)
+_putchar(letter);
+_putchar('\n');
+return (0);
+}
